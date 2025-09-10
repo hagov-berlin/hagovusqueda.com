ab318edf6bc7f5347ecb570cd73587ee689fa05b
Ignacio Heredia <ignacio.nh@gmail.com>
2025-09-10 22:27:48 +0200

    Search

--- src/search.ts
+++ src/search.ts
@@ -1,16 +1,17 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import prisma from "./db";
+import { Subtitle, YoutubeVideo } from "@prisma/client";
 
-export default async function search(req: FastifyRequest, reply: FastifyReply) {
-  const urlQueryParams = req.query as Record<string, string>;
-  const { show, q } = urlQueryParams;
-
-  if (!q) {
-    return reply.status(400).send({ error: "Missing query param 'q'" });
-  }
-
+async function getVideosAndTranscriptIndexes(req: FastifyRequest, options: { limit: number }) {
+  const query = req.query as Record<string, string>;
+  const { show, q } = query;
   let sqlQuery = `
-    SELECT v.id , v.title, v.show, v.date, v.duration
+    SELECT v.id, ts_headline(
+      'spanish',
+      v.transcript,
+      plainto_tsquery('spanish', $1),
+      'StartSel=<mark>, StopSel=</mark>, MaxWords=1000000, MinWords=0, ShortWord=3, HighlightAll=TRUE'
+    ) as matches
     FROM "YoutubeVideo" v
     WHERE to_tsvector('spanish', v.transcript) @@ plainto_tsquery('spanish', $1)
   `;
@@ -28,9 +29,81 @@ export default async function search(req: FastifyRequest, reply: FastifyReply) {
     params.push(shows);
     paramIndex++;
   }
-  sqlQuery += ` ORDER BY v.date DESC LIMIT 50;`;
+  sqlQuery += `LIMIT ${options.limit};`;
 
-  const results = await prisma.$queryRawUnsafe<any[]>(sqlQuery, ...params);
+  const results: { id: string; matches: string }[] = await prisma.$queryRawUnsafe<any[]>(
+    sqlQuery,
+    ...params
+  );
+  return results.map((result) => {
+    const indexes: number[] = [];
+    let accumIndex = 0;
+    result.matches
+      .replace(/<\/mark>/g, "")
+      .split("<mark>")
+      .slice(0, -1)
+      .forEach((preMatch) => {
+        accumIndex += preMatch.length;
+        indexes.push(accumIndex);
+      });
+    return { id: result.id, indexes };
+  });
+}
 
-  return { results };
+function removeUnimportantSubtitles(subtitles: Subtitle[], indexesOriginal: number[]) {
+  const videoIndexes = [...indexesOriginal];
+  let accumIndex = 0;
+  let targetIndex = videoIndexes.shift();
+  const filteredSubtitles = subtitles
+    .sort((subA, subB) => subA.startTime - subB.startTime)
+    .filter((subtitle) => {
+      let weWantThisSubtitle = false;
+
+      if (targetIndex > -1) {
+        accumIndex += subtitle.text.length + 1;
+        if (accumIndex > targetIndex) {
+          weWantThisSubtitle = true;
+          if (videoIndexes.length > 0) {
+            targetIndex = videoIndexes.shift();
+          } else {
+            targetIndex = -1;
+          }
+        }
+      }
+
+      return weWantThisSubtitle;
+    });
+  return filteredSubtitles;
+}
+
+export default async function search(req: FastifyRequest, reply: FastifyReply) {
+  const query = req.query as Record<string, string>;
+
+  if (!query.q) {
+    return reply.status(400).send({ error: "Missing query param 'q'" });
+  }
+
+  const results = await getVideosAndTranscriptIndexes(req, { limit: 50 });
+  const videoIds = results.map((result) => result.id);
+
+  const fullVideos = await prisma.youtubeVideo.findMany({
+    where: {
+      id: { in: videoIds },
+    },
+    include: { subtitles: true },
+    omit: {
+      transcript: true,
+    },
+    orderBy: { date: "desc" },
+  });
+
+  const videosWithTheRightSubtitles = fullVideos.map((video) => {
+    const videoIndexes = results.find((result) => result.id === video.id)?.indexes || [];
+    return {
+      ...video,
+      subtitles: removeUnimportantSubtitles(video.subtitles, videoIndexes),
+    };
+  });
+
+  return { results: videosWithTheRightSubtitles, resultsCapped: false };
 }
