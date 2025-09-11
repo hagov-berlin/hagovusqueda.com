c1e415a21894873e605ba720d0c8957f3118ca82
Ignacio Heredia <ignacio.nh@gmail.com>
2025-09-12 01:51:08 +0200

    Fix show filter

--- src/utils.ts
+++ src/utils.ts
@@ -8,7 +8,7 @@ export function parseQuery(req: FastifyRequest) {
   const page = pageString ? parseInt(pageString) : 1;
 
   let show: string[] = [];
-  if (showString && showString.indexOf(",") > -1) {
+  if (showString) {
     show = showString
       .split(",")
       .slice(0, 20)
